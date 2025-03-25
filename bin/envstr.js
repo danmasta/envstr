b5b38d10eedb5a6d366611f332754177579acafe
Daniel Smith <dannmasta@gmail.com>
2025-03-24 19:06:06 -0700

    update help text, fix argv parsing, use util fns

--- bin/envstr.js
+++ bin/envstr.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
 
-import { optsFromArgv, parseArgv } from 'lo';
-import { stdout } from 'node:process';
+import { ARGV, optsFromArgv, parseArgv } from 'lo';
 import { Envstr } from '../lib/envstr.js';
-import { getStdin } from '../lib/util.js';
+import { readStdin, writeStdout } from '../lib/util.js';
 
 const HELP = `Usage:
-envstr [str] [...options]
+    envstr [...options]
 
 Options:
---string  -s - Text string to parse
---stdin    - - Read input from stdin
---json    -j - Handle input as json
---key     -k - If input is json, parse data at specified key
---quotes  -q - If true add quotes around env values
---newline -n - Which character to use as newline delimeter. Default is '\\n'
---include -i - Which keys to include in output: key1,key2
---exclude -e - Which keys to exclude from output: key3,key4
---caps    -c - If true capitalizes the output key name
---export  -x - If true adds the 'export' keyword in front of each output key
---help    -h - Show this help message
+    --string  -s - Text string to parse
+    --stdin    - - Read input from stdin
+    --json    -j - Handle input as json
+    --key     -k - If input is json, parse data at specified key
+    --quotes  -q - If true add quotes around env values
+    --newline -n - Which character to use as newline delimeter. Default is '\\n'
+    --include -i - Which keys to include in output: key1,key2
+    --exclude -e - Which keys to exclude from output: key3,key4
+    --caps    -c - If true capitalizes the output key name
+    --export  -x - If true adds the 'export' keyword in front of each output key
+    --help    -h - Show this help message
 
-Examples:
-envstr -s '{"KEY1":true,"KEY2":false}' --json --quotes
+Example:
+    envstr -s '{"KEY1":true,"KEY2":false}' --json --quotes
 `;
 
 const args = {
@@ -39,33 +38,28 @@ const args = {
     export: 'x'
 };
 
-let argv = parseArgv();
-let opts = optsFromArgv(args, { argv });
-let { 0: str } = argv._;
-let { help, stdin, json, string } = opts;
-let envstr = new Envstr(opts);
-
-if (string) {
-    str = string;
-}
+const argv = parseArgv(ARGV);
+const opts = optsFromArgv(args, { argv });
+const envstr = new Envstr(opts);
+let { help, stdin, json, string: str } = opts;
 
 if (help) {
-    stdout.write(HELP);
+    writeStdout(HELP);
 } else {
     if (str) {
         if (json) {
-            stdout.write(envstr.parseJsonStr(str));
+            writeStdout(envstr.parseJsonStr(str));
         } else {
-            stdout.write(envstr.parseTableStr(str));
+            writeStdout(envstr.parseTableStr(str));
         }
     } else if (stdin) {
-        str = await getStdin();
+        str = await readStdin();
         if (json) {
-            stdout.write(envstr.parseJsonStr(str));
+            writeStdout(envstr.parseJsonStr(str));
         } else {
-            stdout.write(envstr.parseTableStr(str));
+            writeStdout(envstr.parseTableStr(str));
         }
     } else {
-        stdout.write(HELP);
+        writeStdout(HELP);
     }
 }
