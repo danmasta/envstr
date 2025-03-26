5f36baeee1f05ffd0dc7af5c0930f06ed984b4f7
Daniel Smith <dannmasta@gmail.com>
2025-03-25 20:16:29 -0700

    add version opt, fix pos args, update help msg, use log for output

--- bin/envstr.js
+++ bin/envstr.js
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
 
-import { ARGV, optsFromArgv, parseArgv } from 'lo';
+import { getArgv, optsFromArgv } from 'lo';
 import { Envstr } from '../lib/envstr.js';
-import { readStdin, writeStdout } from '../lib/util.js';
+import { readStdin } from '../lib/util.js';
+import pkg from '../package.json' with { type: 'json' };
 
 const HELP = `Usage:
-    envstr [...options]
+    envstr [str] [...options]
 
 Options:
     --string  -s - Text string to parse
     --stdin    - - Read input from stdin
     --json    -j - Handle input as json
     --key     -k - If input is json, parse data at specified key
-    --quotes  -q - If true add quotes around env values
+    --quotes  -q - If true add quotes around each output value
     --newline -n - Which character to use as newline delimeter. Default is '\\n'
     --include -i - Which keys to include in output: key1,key2
     --exclude -e - Which keys to exclude from output: key3,key4
     --caps    -c - If true capitalizes the output key name
     --export  -x - If true adds the 'export' keyword in front of each output key
+    --version -v - Print the current version
     --help    -h - Show this help message
 
 Example:
-    envstr -s '{"KEY1":true,"KEY2":false}' --json --quotes
-`;
+    envstr -s '{"KEY1":true,"KEY2":false}' --json --quotes`;
 
 const args = {
     string: 's',
@@ -35,31 +36,35 @@ const args = {
     exclude: 'e',
     help: 'h',
     caps: 'c',
-    export: 'x'
+    export: 'x',
+    version: 'v'
 };
 
-const argv = parseArgv(ARGV);
+const argv = getArgv();
 const opts = optsFromArgv(args, { argv });
 const envstr = new Envstr(opts);
-let { help, stdin, json, string: str } = opts;
+const { log } = console;
+
+let { help, version, stdin, json, string: str } = opts;
 
 if (help) {
-    writeStdout(HELP);
+    log(HELP);
+} else if (version) {
+    log(pkg.version);
 } else {
+    if (!str && stdin) {
+        str = await readStdin();
+    }
+    if (!str) {
+        str = argv._.pop();
+    }
     if (str) {
         if (json) {
-            writeStdout(envstr.parseJsonStr(str));
+            log(envstr.parseJsonStr(str));
         } else {
-            writeStdout(envstr.parseTableStr(str));
-        }
-    } else if (stdin) {
-        str = await readStdin();
-        if (json) {
-            writeStdout(envstr.parseJsonStr(str));
-        } else {
-            writeStdout(envstr.parseTableStr(str));
+            log(envstr.parseTableStr(str));
         }
     } else {
-        writeStdout(HELP);
+        log(HELP);
     }
 }
