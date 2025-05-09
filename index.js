ba140f2b87b2e4ca6ac75b35d3d9dc6dd2460251
Daniel Smith <dannmasta@gmail.com>
2025-05-08 17:35:46 -0700

    update exports

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import Envstr from './lib/envstr.js';
+import { Envstr, factory, fromJSON, fromObject, fromPairs, fromTable } from './lib/envstr.js';
+
+const envstr = factory();
 
 export {
-    Envstr as default,
-    Envstr
+    Envstr,
+    envstr,
+    factory,
+    fromJSON,
+    fromObject,
+    fromPairs,
+    fromTable
 };
+
+export default envstr;
