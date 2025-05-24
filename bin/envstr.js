e3ec308436593ee48d26d96c4e1322cb275736c7
Daniel Smith <dannmasta@gmail.com>
2025-05-24 15:52:22 -0700

    update help text

--- bin/envstr.js
+++ bin/envstr.js
@@ -7,22 +7,22 @@ import { readStdin } from '../lib/util.js';
 import pkg from '../package.json' with { type: 'json' };
 
 const HELP = `Usage:
-    envstr [str] [...options]
+    envstr [input] [...options]
 
 Options:
-    --input   -i - Text string to parse
-    --stdin    - - Read input from stdin
-    --json    -j - Handle input as JSON
-    --key     -k - If input is JSON, parse data at specified key
-    --quotes  -q - Add quotes around each output value
-    --newline -n - Which character to use as newline delimeter. Default is '\\n'
-    --pick    -p - Pick which keys to include in output: key1,key2
-    --exclude -e - Exclude keys from output: key3,key4
-    --caps    -c - Capitalize each output key
-    --export  -x - Add the 'export' keyword in front of each output key
-    --prefix  -a - Prefix to add to each output key
-    --version -v - Print the current version
-    --help    -h - Show this help message
+    --input   -i  Text string to parse
+    --stdin    -  Read input from stdin
+    --json    -j  Handle input as JSON
+    --key     -k  If input is JSON, parse data at specified key
+    --quotes  -q  Add quotes around each output value
+    --newline -n  Which character to use as newline delimeter. Default is '\\n'
+    --pick    -p  Pick which keys to include in output: key1,key2
+    --exclude -e  Exclude keys from output: key3,key4
+    --caps    -c  Capitalize each output key
+    --export  -x  Add the 'export' keyword in front of each output key
+    --prefix  -a  Prefix to add to each output key
+    --version -v  Print the current version
+    --help    -h  Show this help message
 
 Example:
     envstr -i '{"KEY1":true,"KEY2":false}' --json --quotes`;
